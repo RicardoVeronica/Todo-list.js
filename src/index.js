377e8ba8ac8580772d172d2253b3688525d67b23
setcain <charlie.bronson00@gmail.com>
2020-12-03 12:31:14 -0600

    adds show error when tweet is empty

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log("hello dev-server");
+// vars
+const $form = document.getElementById("formulario");
+const $tweetsList = document.getElementById("lista-tweets");
+let tweets = [];
+
+// Event listeners
+document.addEventListener("submit", addTweet);
+
+// Functions
+function addTweet(e) {
+  e.preventDefault();
+
+  // textarea where user writes
+  const $userTweet = document.getElementById("tweet").value;
+
+  if ($userTweet === "") showError("Un tweet no puede ir vacio");
+  return;
+}
+
+function showError(msg) {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = msg;
+  paragraph.setAttribute("class", "error");
+
+  const content = document.getElementById("contenido");
+  content.appendChild(paragraph);
+}
